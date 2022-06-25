d473c7395fd6dddfd48501910963738f1b817a46
Rashad Philizaire <rashadp@fb.com>
2022-06-24 19:37:49 -0700

    Initial Commit

---
+++ HackerNewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackerNewsReader
+//
+//  Created by Rashad Philizaire on 6/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
